7e3dfc74df02ae0726367e2b96126a091a02f8dc
booldook <booldook@gmail.com>
2021-07-30 15:02:03 +0900

    02.bulb.html

--- js/02.bulb.js
+++ js/02.bulb.js
@@ -1,3 +1,29 @@
+/* 
+$('.bt-on').on('click', function(){ 
+	console.log('ON');
+});
+
+for(var i=0; i<document.getElementsByClassName('bt-on').length; i++) {
+	document.getElementsByClassName('bt-on')[i].addEventListener('click', function() {
+		console.log('ON');
+	})
+}
+
+for(var i=0; i<document.querySelectorAll('.bt-on').length; i++) {
+	document.querySelectorAll('.bt-on')[i].addEventListener('click', function() {
+		console.log('ON');
+	})
+}
+*/
 
 
 
+// ES5/ES6/jQuery 선택자
+// document.getElementById('idName') -> html
+// document.getElementsByClassName('className') -> Array[html, html]
+// document.getElementsByTagName('tagName') -> Array[html, html]
+
+// document.querySelector('.w100') -> html
+// document.querySelectorAll('.w100') -> Array[html, html]
+
+// $('.w100') -> jQuery
