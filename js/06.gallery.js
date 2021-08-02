f38f32ece1ac81fb65ffd7d9aec8eb14d47181fe
booldook <booldook@gmail.com>
2021-08-02 16:08:04 +0900

    06.gallery 서서히 나타나기

--- js/06.gallery.js
+++ js/06.gallery.js
@@ -14,11 +14,25 @@ for(var i=0, html; i<foods.length; i++) {
 	html += '</li>';
 	$('.list-wrap').append(html);
 }
+/*
+ * 1차 버전
 $('.list-wrap .thumb').click(function() {
+	console.log( $(this).parent() ); // this의 부모
+	console.log( $(this).parents() ); // this의 모든 조상(배열)
 	$('.stage-wrap .big').attr('src', $(this).attr('src'));
 	$('.stage-wrap .name').html($(this).attr('alt'));
-	// console.log( $(this).parent() ); // this의 부모
-	// console.log( $(this).parents() ); // this의 모든 조상(배열)
+	$('.list-wrap .list').removeClass('active');
+	$(this).parent().addClass('active');
+});
+*/
+
+// 2차 버전
+$('.list-wrap .thumb').click(function() {
+	var html = '<div class="hover"><img src="'+$(this).attr('src')+'" class="w100"></div>';
+	$('.stage-wrap').append(html);
+	$('.stage-wrap .hover').stop().hide();
+	$('.stage-wrap .hover').stop().fadeIn(1000);
+	$('.stage-wrap .name').html($(this).attr('alt'));
 	$('.list-wrap .list').removeClass('active');
 	$(this).parent().addClass('active');
 });
