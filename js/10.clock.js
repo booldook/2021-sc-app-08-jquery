bf12da1529c039beb0ab9acfdc99e9f0a0bad987
booldook <booldook@gmail.com>
2021-08-03 12:21:42 +0900

    10.clock

--- js/10.clock.js
+++ js/10.clock.js
@@ -13,4 +13,12 @@ console.log(d.getSeconds());	// 0 ~ 59
 var nowDate = d.getFullYear() + '-' + zp(d.getMonth() + 1) + '-' + zp(d.getDate()) + ' ' + zp(d.getHours()) + ':' + zp(d.getMinutes()) + ':' + zp(d.getSeconds()); 
 console.log(nowDate); // ISO 시간표기
 
-
+setInterval(onInterval, 1000);
+function onInterval() {
+	var yoil = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; 
+	var d = new Date();
+	var date = d.getFullYear() + '-' + zp(d.getMonth() + 1) + '-' + zp(d.getDate());
+	var time = zp(d.getHours()) + ':' + zp(d.getMinutes()) + ':' + zp(d.getSeconds());
+	document.querySelector('.date-wp').innerHTML = date + '. ' + yoil[d.getDay()];
+	document.querySelector('.time-wp').innerHTML = time;
+}
