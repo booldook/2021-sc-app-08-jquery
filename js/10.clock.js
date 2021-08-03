b6ce556816eddcf2e7b0405f682942ec0566f6a3
booldook <booldook@gmail.com>
2021-08-03 13:19:30 +0900

    11.animation.html

--- js/10.clock.js
+++ js/10.clock.js
@@ -9,16 +9,22 @@ console.log(d.getDay());			// 0(Sunday) ~ 6
 console.log(d.getHours());		// 24시간제(0 ~ 23)
 console.log(d.getMinutes());	// 0 ~ 59
 console.log(d.getSeconds());	// 0 ~ 59
+console.log(d.getTime());			// timestamp(1970.01.01. 00:00:00) - 기준으로 현 시점까지의 흘러온 초(16억), 밀리초(1조6천): 자바스크립트: ms, 자바: s
 
 var nowDate = d.getFullYear() + '-' + zp(d.getMonth() + 1) + '-' + zp(d.getDate()) + ' ' + zp(d.getHours()) + ':' + zp(d.getMinutes()) + ':' + zp(d.getSeconds()); 
 console.log(nowDate); // ISO 시간표기
 
 setInterval(onInterval, 1000);
 function onInterval() {
+	/*
 	var yoil = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; 
 	var d = new Date();
 	var date = d.getFullYear() + '-' + zp(d.getMonth() + 1) + '-' + zp(d.getDate());
 	var time = zp(d.getHours()) + ':' + zp(d.getMinutes()) + ':' + zp(d.getSeconds());
 	document.querySelector('.date-wp').innerHTML = date + '. ' + yoil[d.getDay()];
 	document.querySelector('.time-wp').innerHTML = time;
+	*/
+	document.querySelector('.date-wp').innerHTML = moment().format('YYYY-MM-DD. ddd');
+	document.querySelector('.time-wp').innerHTML = moment().format('HH:mm:ss');
+	// document.querySelector('.timestamp-wp').innerHTML = new Date().getTime();
 }
---
+++ js/11.animation.js
@@ -0,0 +1,34 @@
+/***************** 전역설정 ******************/
+
+
+/***************** 함수선언 ******************/
+
+
+/***************** 이벤트콜백 ******************/
+function onFade(_el, chk) {
+	var el = document.querySelector(_el);
+	var opacity = getComputedStyle(el).getPropertyValue('opacity');
+	if(chk === 'show') {
+		
+	}
+	else {
+
+	}
+}
+
+function onShow() {
+	var interval;
+	var opacity = 1;
+	// console.log( document.getElementsByClassName('box') );
+	interval = setInterval(onFade, 1000, '.box', 'show')
+}
+
+function onHide() {
+
+
+}
+
+/***************** 이벤트등록 ******************/
+document.querySelector('.bt-show').addEventListener('click', onShow);
+document.querySelector('.bt-hide').addEventListener('click', onHide);
+
